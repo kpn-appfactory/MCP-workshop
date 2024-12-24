e37b73cf54463030131c298bbbf9fa5efcae801d
Jeroen van Gemert <github@jeroen.van.gemert.net>
2024-12-24 10:11:28 +0100

    Added vardemo app

---
+++ apps/vardemo/image-content/scripts/workshop.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+import socket
+import time
+import os
+import datetime
+
+def main():
+    # Define the port on which the server will listen
+    port = int(os.getenv('PORT', 8080))
+    requests_file = os.getenv('REQUESTS_FILE', '/data/requests.txt')
+    envs = []
+    cm_vars = []
+    for name, value in os.environ.items():
+        envs.append({'name': name, 'value': value})
+        if name.startswith('CM_VAR_'):
+            cm_vars.append({'name': name, 'value': value})
+
+    # Sort the CM_VAR_ vars by name
+    envs = sorted(envs, key=lambda x: x['name'])
+    cm_vars = sorted(cm_vars, key=lambda x: x['name'])
+
+    # Create a socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Get the hostname of the server
+    host = socket.gethostname()
+
+    # Bind the socket to the host and port
+    server_socket.bind(("0.0.0.0", port))
+
+    # Listen for incoming connections
+    server_socket.listen(5)
+
+    log(f"Container is listening on port { port}")
+
+    while True:
+        # Establish connection with client
+        client_socket, addr = server_socket.accept()
+
+        # Connection received from client
+        log("Got a connection from %s" % str(addr))
+
+        # Data received from client
+        data = client_socket.recv(1024)
+        data_lines = data.decode('ascii').split('\n')
+        get_request = None
+        source_ip = None
+        for line in data_lines:
+            if line.startswith('GET '):
+                get_request = line.split(' ')[1].split('?')[0]
+                get_request_path = get_request.split('/')
+                get_request_path = [x for x in get_request_path if x]
+            if line.startswith('X-Forwarded-For: ') and not source_ip:
+                source_ip = line.split(' ')[1]
+            if line.startswith('X-Real-IP: '):
+                source_ip = line.split(' ')[1]
+
+        if not source_ip:
+            source_ip = str(addr[0])
+        source_port = str(addr[1])
+
+        log(f"Received data from client:\n{data.decode('ascii')}\n")
+
+        body =  f"Time in container: {str(time.ctime())}\n"
+        body += f"Requests received: {requests(requests_file)}\n"
+        body += f"Hostname: {host}\n"
+        body += f"Container port: {port}\n"
+        body += f"Client source ip: {source_ip}\n"
+        body += f"Client source port: {source_port}\n"
+        body += f"GET request: {get_request}\n"
+        body += "\n"
+        if len(get_request_path) == 1 and get_request_path[0].lower() == "env":
+            body += "Environment variables:\n"
+            for env in envs:
+                body += f"{env['name']}: {env['value']}\n"
+        else:
+            if cm_vars:
+                for cm_var in cm_vars:
+                    body += f"{cm_var['name']}: {cm_var['value']}\n"
+            else:
+                body += "No CM_VAR_ vars found\n"
+        response_headers = {
+            'Content-Type': 'text/text; encoding=utf8',
+            'Content-Length': len(body),
+            'Connection': 'close',
+        }
+
+        response_headers_raw = ''.join('%s: %s\r\n' % (k, v) for k, v in response_headers.items())
+        response_proto = 'HTTP/1.1'
+        response_status = '200'
+        response_status_text = 'OK' # this can be random
+
+        # sending all this stuff
+        response = f"{response_proto} {response_status} {response_status_text}\r\n"
+        client_socket.sendall(response.encode())
+        client_socket.sendall(response_headers_raw.encode())
+        client_socket.sendall(b'\r\n') # to separate headers from body
+        client_socket.send(body.encode(encoding="utf-8"))
+
+        # Close the connection
+        client_socket.close()
+
+
+def requests(requests_file:str) -> int:
+    if not os.path.exists(requests_file):
+        with open(requests_file, 'w') as f:
+            f.write('0')
+    with open(requests_file, 'r') as f:
+        requests = int(f.read())
+    requests += 1
+    with open(requests_file, 'w') as f:
+        f.write(str(requests))
+    return requests
+
+
+def log(msg):
+    print(f'{now()} {msg}')
+
+
+def now():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+if __name__ == '__main__':
+    main()
+
