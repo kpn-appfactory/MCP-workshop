097ed415838fb45281d84d53ea3bed0c77f285b2
Jeroen van Gemert <github@jeroen.van.gemert.net>
2025-01-06 14:53:01 +0100

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Update OS & install docker
+sudo apt update
+sudo apt upgrade
+sudo apt install docker.io -y
+
+# Give current user access to docker
+sudo usermod -aG docker $(whoami)
+
+# Install K3S
+curl -sfL https://get.k3s.io | sh -
+
+# Allow all users to access Kubernetes (k3s)
+sudo chmod +r /etc/rancher/k3s/k3s.yaml
+
+# Configure bash completion and alias for kubectl
+LINE='source <(kubectl completion bash)'
+grep "${LINE}" ~/.bashrc > /dev/null
+if [ ${?} -gt 0 ]
+then
+    echo "${LINE}" >> ~/.bashrc
+    echo 'alias k=kubectl' >> ~/.bashrc
+    echo 'complete -o default -F __start_kubectl k' >> ~/.bashrc
+fi
+
+# Install k9s
+wget https://github.com/derailed/k9s/releases/download/v0.32.7/k9s_linux_amd64.deb -O /tmp/k9s_linux_amd64.deb && sudo apt install /tmp/k9s_linux_amd64.deb && rm /tmp/k9s_linux_amd64.deb
+
+# Link to kube config for k9s
+ln -fs /etc/rancher/k3s/k3s.yaml ~/.kube/config
+
+echo
+echo "!!! Please logout and login again to make use of new group membership !!!"
+ 
